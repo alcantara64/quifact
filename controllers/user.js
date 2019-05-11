a1ec4f8399ee0f3eb1c35369ff769cc934adb3eb
Emmanuel Agahiu <alcantara@Emmanuels-MacBook-Air.local>
2019-05-11 06:01:22 +0100

    display flash message

--- controllers/user.js
+++ controllers/user.js
@@ -47,7 +47,7 @@ exports.postLogin = (req, res, next) => {
     req.logIn(user, (err) => {
       if (err) { return next(err); }
       req.flash('success', { msg: 'Success! You are logged in.' });
-      res.redirect(req.session.returnTo || '/');
+      res.redirect('account' || '/account');
     });
   })(req, res, next);
 };
